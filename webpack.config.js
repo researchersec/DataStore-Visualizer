a74f86d0a77426d92d04db97d6a5a179f3cd67a3
researchersec <109282836+researchersec@users.noreply.github.com>
2024-06-02 01:12:02 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -18,4 +19,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html'
+    }),
+  ],
 };
